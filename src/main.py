c88514ca97ca14c15b2f8808bf41c24a7b1505c7
Michael Joubert <mike@michaeljoubert.com>
2024-08-07 19:24:15 +0100

    Add the parent_node into the json

--- src/main.py
+++ src/main.py
@@ -7,14 +7,16 @@ class ElectricalNetworkGenerator:
         self.num_levels = num_levels
         self.num_children_per_node = num_children_per_node
         self.node_id = 1
+        self.boards = []
 
-    def generate_board(self, level, position, parent_phase):
+    def generate_board(self, level, position, parent_phase, parent_id=None):
         if parent_phase == "Single-phase":
             str_phase = "Single-phase"
         else: 
             str_phase = random.choice(["Single-phase", "Three-phase"])
         board = {
             "node_id": self.node_id,
+            "parent_node_id": parent_id,
             "name": f"DistributionBoard: Level: {level}",
             "circuits": [],
             "phase": str_phase
@@ -33,7 +35,7 @@ class ElectricalNetworkGenerator:
                 circuit = {
                     "circuit_number": cct_index,
                     "circuit_type": circuit_type,
-                    "child_board": None
+                    "child_node_id": None
                 }
 
                 str_print = (f"circuit-{cct_index} phase-{str_phase}")
@@ -42,19 +44,18 @@ class ElectricalNetworkGenerator:
                 if num_children > 0:
                     print(Indent("|  ", 10*level))
                     print(Indent("_______", 10*level))
-                    child_board = self.generate_board(level + 1, cct_index, str_phase)
-                    circuit["child_board"] = child_board
+                    child_board = self.generate_board(level + 1, cct_index, str_phase, current_id)
+                    circuit["child_node_id"] = child_board["node_id"]
 
                 board["circuits"].append(circuit)
 
+        self.boards.append(board)
         return board
 
     def generate_network_data(self):
-        network_data = []
         for root_position in range(self.num_roots):
-            root_board = self.generate_board(1, str(root_position), "Three-phase")
-            network_data.append(root_board)
-        return network_data
+            self.generate_board(1, str(root_position), "Three-phase")
+        return self.boards
 
 def Indent(char, num_indents):
     """
@@ -77,7 +78,7 @@ def charprint(char, num_times):
 
 def main():
     # Parameters
-    num_roots = 2
+    num_roots = 1
     num_levels = 3
     num_children_per_node = 3
 
